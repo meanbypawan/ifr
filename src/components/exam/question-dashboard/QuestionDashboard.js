a3c2aa076ebb58fd9a60ceac14248e61e86454a2
mean <meanstack4@gmail.com>
2023-10-25 17:14:54 +0530

    work on result generation

--- src/components/exam/question-dashboard/QuestionDashboard.js
+++ src/components/exam/question-dashboard/QuestionDashboard.js
@@ -8,8 +8,36 @@ import { toast } from "react-toastify";
 export default function QuestionDashBoard(){
   const {userId} = useSelector((store)=>store.user);
   const [questionList,setQuestionList] = useState([]);
+  
   const[activeQuestionList,setActiveQuestionList] = useState("English");
+  
   const [subjectList,setSubjectList] = useState(["English","Hindi","General Knowledge","Computer Basic","Quantitative Aptitude","Logical Resoning"]);
+  
+  const [questionPaper,setQuestionPaper] = useState(JSON.parse(localStorage.getItem("question-list")));
+
+  const changeTab = (scrollContainerId,subject)=>{
+    setActiveQuestionList(subject);
+    let scrollContainer = document.querySelector("#"+scrollContainerId);    
+    scrollContainer.scrollTop = 0;
+  }
+  const styles = {
+    selected:{
+      backgroundColor: "#EA3F3F",
+      color:"white",
+      width:"20px",
+      height:"20px",
+      borderRadius:"50%",
+      fontSize:"10px"
+    },
+    notSelected:{
+      backgroundColor: "#E1DFDF",
+      color: "black",
+      width:"20px",
+      height:"20px",
+      borderRadius:"50%",
+      fontSize:"10px"
+    }
+  };
   const [time, setTime] = useState(90*60);
  
   useEffect(()=>{
@@ -18,23 +46,40 @@ export default function QuestionDashBoard(){
   const loadQuestionsPaper = async ()=>{
    try{ 
     const response = await axios.post("http://localhost:3001/paper/generate",{userId});
-    console.log(response.data.questionsList[0].English);
     setQuestionList([...response.data.questionsList]);
+    console.log("Inside load question paper...........");
+    console.log(questionList);
+    saveQuestion(response.data.questionsList);
+    setTime(localStorage.getItem("timer")*1);
    }
    catch(err){
     toast.error("Oops! something went wrong..");
    } 
   }
+
+  const saveQuestion = (questionList)=>{
+    //questionList = JSON.parse(questionList);
+    if(!localStorage.getItem("question-list")){
+       let updateQuestionList = {"English":[],"Hindi":[],"General Knowledge":[],"Computer Basic":[],"Quantitative Aptitude":[],"Logical Resoning":[]};
+       for(let key in questionList[0])
+          for(let question of questionList[0][key]){
+            delete question.Answer;
+            updateQuestionList[key].push(question);
+          }
+        localStorage.setItem("question-list",JSON.stringify(updateQuestionList));
+        setQuestionPaper(updateQuestionList);
+    }
+  } 
   useEffect(() => {
     const timer = setInterval(() => {
       if (time > 0) {
         setTime(time - 1);
+        localStorage.setItem("timer",""+(time-1));
       } else {
         clearInterval(timer);
         // Handle timer completion here
       }
     }, 1000);
-
     return () => {
       clearInterval(timer); // Cleanup the timer when the component unmounts
     };
@@ -47,7 +92,8 @@ export default function QuestionDashBoard(){
             <div className="col-md-9">
               <Header/>
               <div className="container-fluid bg-white mt-2 pt-1 pb-1">
-              <QuestionTab  userId={userId} questionList={questionList} setQuestionList={setQuestionList} activeQuestionList={activeQuestionList} setActiveQuestionList={setActiveQuestionList}/>
+              {questionList.length!=0 && questionPaper!=null?<QuestionTab changeTab={changeTab} userId={userId} questionList={questionList} setQuestionList={setQuestionList} activeQuestionList={activeQuestionList} setActiveQuestionList={setActiveQuestionList} subjectList={subjectList} setSubjectList={setSubjectList} questionPaper={questionPaper} setQuestionPaper={setQuestionPaper}/> : <p>Data loading....</p>}  
+              
               </div>
             </div>  
             <div className="col-md-3" style={{backgroundColor:"white"}}>
@@ -59,7 +105,7 @@ export default function QuestionDashBoard(){
                 {subjectList.map((subject,index)=><div key={index}>
                   <div>{subject}</div>
                   <div className="row pl-3">
-                   {questionList[0]?.[subject].map((question,index)=><span className="d-flex ml-2 mt-1 justify-content-center align-items-center" style={{width:"20px", height:"20px", borderRadius:"50%", backgroundColor:"#E1DFDF", fontSize:"10px"}}>{index+1}</span>
+                   {questionList[0]?.[subject].map((question,index)=><span key={index} className="d-flex ml-2 mt-1 justify-content-center align-items-center" style={(questionPaper?.[subject].find((obj)=>{return obj.Id == question.Id}).AnswerKey!='null')? styles.selected:styles.notSelected}>{index+1}</span>
                   )}
                   </div>
                   <hr/>
--- src/components/exam/question-tab/QuestionTab.js
+++ src/components/exam/question-tab/QuestionTab.js
@@ -1,30 +1,77 @@
+import React, { useState } from 'react';
 import './QuestionTab.css';
-export default function QuestionTab({userId,questionList,setQuestionList,activeQuestionList,setActiveQuestionList}){
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+function QuestionTab({changeTab,questionPaper,setQuestionPaper,userId,questionList,setQuestionList,activeQuestionList,setActiveQuestionList,subjectList,setSubjectList}){
+    console.log(questionPaper);
+
+    const saveAnswer = (id,answerKey)=>{
+      let questionList = JSON.parse(localStorage.getItem("question-list"));
+      let updatedQuestionList = {...questionList};
+      let targetedQuestion = updatedQuestionList[activeQuestionList].findIndex((question)=>question.Id==id);
+      updatedQuestionList[activeQuestionList][targetedQuestion].AnswerKey = answerKey;
+      localStorage.setItem("question-list",JSON.stringify(updatedQuestionList));  
+      setQuestionPaper(updatedQuestionList);
+    }
+    const submitTest = async ()=>{
+       let questionList = localStorage.getItem("question-list");      
+       questionList = questionList && JSON.parse(questionList);
+       let response = await axios.post("http://localhost:3001/paper/submit",{questionList,userId});
+       if(response.data.score >=50)
+        toast.success("Passed : Total Score  is : "+response.data.score);
+       else
+        toast.error("Failed : Total Score is : "+response.data.score);
+       
+      localStorage.clear();  
+    }
     return <>
+    <ToastContainer/>
       <div className="container-fluid">
         <div className="row">
            <div className="col-md-12 tabs d-flex justify-content-between">
-              <button onClick={()=>setActiveQuestionList("English")} className={(activeQuestionList == "English") ? "active":""}>English</button>
-              <button onClick={()=>setActiveQuestionList("Hindi")} className={(activeQuestionList == "Hindi") ? "active":""}>Hindi</button>
-              <button onClick={()=>setActiveQuestionList("General Knowledge")} className={(activeQuestionList == "General Knowledge") ? "active":""}>G.K</button>
-              <button onClick={()=>setActiveQuestionList("Computer Basic")} className={(activeQuestionList == "Computer Basic") ? "active":""}>Computer</button>
-              <button onClick={()=>setActiveQuestionList("Quantitative Aptitude")} className={(activeQuestionList == "Quantitative Aptitude") ? "active":""}>Math</button>
-              <button onClick={()=>setActiveQuestionList("Logical Resoning")} className={(activeQuestionList == "Logical Resoning") ? "active":""}>L.R</button>
+              <button onClick={()=>changeTab('question-section',"English")} className={(activeQuestionList == "English") ? "active":""}>English</button>
+                <button onClick={()=>changeTab('question-section',"Hindi")} className={(activeQuestionList == "Hindi") ? "active":""}>Hindi</button>
+              <button onClick={()=>changeTab('question-section',"General Knowledge")} className={(activeQuestionList == "General Knowledge") ? "active":""}>G.K</button>
+              <button onClick={()=>changeTab('question-section',"Computer Basic")} className={(activeQuestionList == "Computer Basic") ? "active":""}>Computer</button>
+              <button onClick={()=>changeTab('question-section',"Quantitative Aptitude")} className={(activeQuestionList == "Quantitative Aptitude") ? "active":""}>Math</button>
+              <button onClick={()=>changeTab('question-section',"Logical Resoning")} className={(activeQuestionList == "Logical Resoning") ? "active":""}>L.R</button>
            </div>
         </div>
         <div className="row">
-          <div className="col-12 question-section p-5">
-            {questionList[0]?.[activeQuestionList].map((question,index)=><div className="questions">
+          <div id="question-section" className="col-12 question-section p-5">
+            {questionPaper[activeQuestionList].map((question,index)=><div className="questions" key={index}>
               <p style={{fontWeight:"bold"}}>Q.{index+1} {question.Question}</p>
-              <p><input name={"answerKey"+index} type='radio' className="mr-2"/>{question.A}</p>
-              <p><input name={"answerKey"+index} type='radio' className="mr-2"/> {question.B}</p>
-              <p><input name={"answerKey"+index} type='radio' className="mr-2"/> {question.C}</p>
-              <p><input name={"answerKey"+index} type='radio' className="mr-2"/> {question.D}</p>
+              <p><input checked={questionPaper[activeQuestionList].find((obj)=>obj.Id==question.Id).AnswerKey=="A"} onChange={()=>saveAnswer(question.Id,"A")} id={activeQuestionList+question.A}  name={activeQuestionList+question._id} type='radio' className="mr-2"/><label htmlFor={activeQuestionList+"A"}>{question.A}</label></p>
+              <p><input checked={questionPaper[activeQuestionList].find((obj)=>obj.Id==question.Id).AnswerKey=="B"} onChange={()=>saveAnswer(question.Id,"B")} id={activeQuestionList+question.B}  name={activeQuestionList+question._id} type='radio' className="mr-2"/><label htmlFor={activeQuestionList+"B"}>{question.B}</label></p>
+              <p><input checked={questionPaper[activeQuestionList].find((obj)=>obj.Id==question.Id).AnswerKey=="C"} onChange={()=>saveAnswer(question.Id,"C")} id={activeQuestionList+question.C}  name={activeQuestionList+question._id} type='radio' className="mr-2"/><label htmlFor={activeQuestionList+"C"}>{question.C}</label></p>
+              <p><input checked={questionPaper[activeQuestionList].find((obj)=>obj.Id==question.Id).AnswerKey=="D"} onChange={()=>saveAnswer(question.Id,"D")} id={activeQuestionList+question.D}  name={activeQuestionList+question._id} type='radio' className="mr-2"/><label htmlFor={activeQuestionList+"D"}>{question.D}</label></p>
               <hr/> 
             </div>)}
             
-          </div>    
-        </div>       
+          </div>  
+        </div>    
+      </div>
+      <div className="container-fluid mt-3">
+        <div className="row">
+          <div className="col-12 question-tracking-section d-flex justify-content-around align-items-center">
+            <div>
+              <span className="mr-2" style={{width:"15px", height:"15px", borderRadius:"50%",display:"inline-block",backgroundColor:"#EA3F3F"}}></span> Answerd
+            </div>
+            <div>
+            <span className="mr-2" style={{width:"15px", height:"15px",display:"inline-block", borderRadius:"50%",backgroundColor:"#E1DFDF"}}></span> Not answered
+            </div>
+            <div><span className="mr-2" style={{width:"15px", height:"15px",display:"inline-block", borderRadius:"50%",backgroundColor:"#E77C34"}}></span>Marked for review</div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row" style={{padding:"0px"}}>
+          <div style={{padding:"0px"}} className="col-12 question-footer d-flex justify-content-center align-items-center">
+            <button onClick={()=>submitTest()} id="submit-button" className="btn btn-outline-secondary w-100 p-2">End Test</button>
+          </div>
+        </div>
       </div>
     </>
 }
+
+export default QuestionTab;
