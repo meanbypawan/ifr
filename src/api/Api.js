68d8d53aa449e416e654bf0b006f4a78dded564b
mean <meanstack4@gmail.com>
2026-02-23 11:00:37 +0530

    final update

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,15 +1,16 @@
+// https://exam-backend-mhvf.onrender.com
 export default {
-    USER_SIGN_IN: "http://localhost:3001/user/signin",
-    GENERATE_PAPER: "http://localhost:3001/paper/generate",
-    SUBMIT_EXAM: "http://localhost:3001/paper/submit",
-    ADMIN_SIGN_IN:"http://localhost:3001/admin/login",
-    CREATE_EXAM: "http://localhost:3001/exam/create",
-    EXAM_CODE: "http://localhost:3001/exam/code",
-    UPLOAD_USER: "http://localhost:3001/user/upload-user",
-    UPLOAD_QUESTION: "http://localhost:3001/question/upload-question",
-    EXAM_LIST: "http://localhost:3001/exam/list",
-    REMOVE_EXAM: "http://localhost:3001/exam/remove",
-    GENERATE_PASSWORD: "http://localhost:3001/exam/generate-password", 
-    CHANGE_EXAM_STATUS: "http://localhost:3001/exam/change-status",
-    LOAD_DATA_SHEET: "http://localhost:3001/user/data-sheet"
+    USER_SIGN_IN: "https://exam-backend-mhvf.onrender.com/user/signin",
+    GENERATE_PAPER: "https://exam-backend-mhvf.onrender.com/paper/generate",
+    SUBMIT_EXAM: "https://exam-backend-mhvf.onrender.com/paper/submit",
+    ADMIN_SIGN_IN:"https://exam-backend-mhvf.onrender.com/admin/login",
+    CREATE_EXAM: "https://exam-backend-mhvf.onrender.com/exam/create",
+    EXAM_CODE: "https://exam-backend-mhvf.onrender.com/exam/code",
+    UPLOAD_USER: "https://exam-backend-mhvf.onrender.com/user/upload-user",
+    UPLOAD_QUESTION: "https://exam-backend-mhvf.onrender.com/question/upload-question",
+    EXAM_LIST: "https://exam-backend-mhvf.onrender.com/exam/list",
+    REMOVE_EXAM: "https://exam-backend-mhvf.onrender.com/exam/remove",
+    GENERATE_PASSWORD: "https://exam-backend-mhvf.onrender.com/exam/generate-password", 
+    CHANGE_EXAM_STATUS: "https://exam-backend-mhvf.onrender.com/exam/change-status",
+    LOAD_DATA_SHEET: "https://exam-backend-mhvf.onrender.com/user/data-sheet"
 }
